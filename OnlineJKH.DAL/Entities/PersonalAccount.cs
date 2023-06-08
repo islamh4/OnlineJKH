e83451d82b42888669c65710147d594dda9a07af
islamh4 <islamnabeev22@gmail.com>
2023-06-08 23:36:20 +0300

    [JKH-013] Доработал дизайн вкладок квитанция, лицевые счета, пользователи.

--- OnlineJKH.DAL/Entities/PersonalAccount.cs
+++ OnlineJKH.DAL/Entities/PersonalAccount.cs
@@ -12,11 +12,11 @@ namespace OnlineJKH.DAL.Entities
     public class PersonalAccount
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "Лицевой счёт обязательно!")]
         [Display(Name = "Лицевой счёт")]
         [RegularExpression(@"\d{10}", ErrorMessage = "Длина номера составляет 10 символов, пожалуйста, введите корректно.")]
+        [Required(ErrorMessage = "Лицевой счёт обязателен!")]
         public string Number { get; set; }
         public int UserId { get; set; }
-        public virtual required User User { get; set; }
+        public virtual User? User { get; set; }
     }
 }
